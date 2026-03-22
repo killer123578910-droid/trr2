a5181381835845607b7feec645c550afb5c2f212
Khuong <killer123578910@gmail.com>
2026-03-22 14:41:31 +0700

    stack

--- dfsstack.cpp
+++ dfsstack.cpp
@@ -5,12 +5,14 @@ void dfs(int i,vector<vector<int>> ar,vector<bool> &vs,int n){
     vs[i]=true;
     stack<int> st;
     st.push(i);
+    cout<<i<<" ";
     while(!st.empty()){
         int top=st.top();
         st.pop();
         for(int j=1;j<=n;j++){
             if(ar[top][j]&&!vs[j]){
                 cout<<j<<" ";
+                vs[j]=true;
                 st.push(top);
                 st.push(j);
                 break;
