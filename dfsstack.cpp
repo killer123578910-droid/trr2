5a7bf2afb5f7af15b1c5f823c4c2a0905ec2bb03
Khuong <killer123578910@gmail.com>
2026-03-22 14:34:27 +0700

    e

--- dfsstack.cpp
+++ dfsstack.cpp
@@ -1,32 +1,31 @@
 #include<bits/stdc++.h>
 using namespace std;
-
+int n;
+void dfs(int i,vector<vector<int>> ar,vector<bool> &vs,int n){
+    vs[i]=true;
+    stack<int> st;
+    st.push(i);
+    while(!st.empty()){
+        int top=st.top();
+        st.pop();
+        for(int j=1;j<=n;j++){
+            if(ar[top][j]&&!vs[j]){
+                cout<<j<<" ";
+                st.push(top);
+                st.push(j);
+                break;
+            }
+        }
+    }
+}
 int main(){
-    int n;
     cin>>n;
     vector<vector<int>> ar(n+1,vector<int>(n+1));
-    vector<bool> visite(n+1,false);
+    vector<bool> vs(n+1,0);
     for(int i=1;i<=n;i++){
         for(int j=1;j<=n;j++){
             cin>>ar[i][j];
         }
     }
-    stack<int> st;
-    st.push(1);
-    cout<<1<<" ";
-    visite[1]=true;
-    while(!st.empty()){
-        int u=st.top();st.pop();
-            for(int k=1;k<=n;k++){
-                if(ar[u][k]&&!visite[k]){
-                    visite[k]=true;
-                    cout<<k<<" ";
-                    st.push(u);
-                    st.push(k);
-                    break;
-                }
-            }
-        }
-        return 0;
+    dfs(1,ar,vs,n);
 }
-   
