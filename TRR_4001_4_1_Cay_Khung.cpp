14e4ae73a6ee4a847d6ebbd188ee141f9c75e069
toibingu <killer123578910@gmail.com>
2026-05-16 17:00:21 +0700

    eee

---
+++ TRR_4001_4_1_Cay_Khung.cpp
@@ -0,0 +1,59 @@
+#include<bits/stdc++.h>
+using namespace std;
+int t,n,u;
+ifstream in("CK.INP");
+ofstream out("CK.OUT");
+vector<pair<int,int>> bfs(int i,vector<int>&vs,vector<vector<int>>&ar){
+    vector<pair<int,int>> kq;
+    queue<int> q;
+    q.push(i);
+    vs[i]=1;
+    while(!q.empty()){
+        int front=q.front();q.pop();
+        for(int j=1;j<=n;j++){
+            if(ar[front][j]&&!vs[j]){
+                kq.push_back({front,j});
+                vs[j]=1;
+                q.push(j);
+            }
+        }
+    }
+    return kq;
+}
+void dfs(int i,vector<vector<int>>&ar,vector<int>&vs,vector<pair<int,int>>&kq){
+    vs[i]=1;
+    for(int j=1;j<=n;j++){
+        if(ar[i][j]&&!vs[j]){
+            kq.push_back({i,j});
+            dfs(j,ar,vs,kq);
+        }
+    }
+}
+
+int main(){
+    int t;
+    in>>t>>n>>u;
+    vector<vector<int>> ar(n+1,vector<int>(n+1));
+    vector<int> vs(n+1,0);
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n;j++){
+            in>>ar[i][j];
+        }
+    }
+    if(t==1){
+        vector<pair<int,int>> kq;
+        dfs(u,ar,vs,kq);
+        out<<kq.size()<<endl;
+        for(auto x:kq){
+            out<<x.first<<" "<<x.second<<endl;
+        }
+    }else{
+        vector<pair<int,int>> kq=bfs(u,vs,ar);
+        out<<kq.size()<<endl;
+        for(auto x:kq){
+            out<<x.first<<" "<<x.second<<endl;
+        }
+    }
+
+    return 0;
+}
